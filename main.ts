375119f1ddeaaa99bdb2b6e20ee74c9778fc329f
Jakub Kreisinger <jakub.kreisinger.024@pslib.cz>
2024-12-13 13:26:29 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,40 @@
-
-let pasek: neopixel.Strip  = neopixel.create(DigitalPin.P8, 24, NeoPixelMode.RGB)
+let pasek: neopixel.Strip = neopixel.create(DigitalPin.P8, 24, NeoPixelMode.RGB)
 pasek.showRainbow(1, 360)
-basic.forever(function() {
-pasek.rotate(1)
-pasek.show()
 
-basic.pause(100)
+let rotace = 0
+let zastaveni = false
+let smer = false
 
-}
-)
+basic.forever(function () {
+    
+    if (input.buttonIsPressed(Button.AB)) {
+        smer = !smer 
+        basic.pause(500) 
+    }
+
+    if (input.logoIsPressed()) {
+        zastaveni = !zastaveni
+        basic.pause(500)
+    }
+    
+    if (!zastaveni) { 
+        pasek.rotate(1)
+        pasek.show()
+        basic.pause(rotace)
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (rotace > 10) {
+        rotace -= 10
+        basic.showNumber(rotace / 100)
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    if (rotace < 1000) {
+        rotace += 10
+        basic.showNumber(rotace / 100)
+    }
+})
+
